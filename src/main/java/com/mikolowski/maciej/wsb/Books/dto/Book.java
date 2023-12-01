ae7bd6eba11ccab8ee03be34b9537ff559e9c1cb
wsbGda76772 <gdx167494@student.gdansk.merito.pl>
2023-12-01 18:24:46 +0100

    Connection to h2 database, bookRepository and basic information added to README.md

--- src/main/java/com/mikolowski/maciej/wsb/Books/dto/Book.java
+++ src/main/java/com/mikolowski/maciej/wsb/Books/dto/Book.java
@@ -1,16 +1,29 @@
 package com.mikolowski.maciej.wsb.Books.dto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotNull
     private String title;
 
+    @NotNull
     private String author;
 
+    @NotNull
+    @Positive
     private Float price;
 
     public Float getPrice() {
